ff92fa24ff42b27bc9ba4441fde161ee059e6cc2
lsabit <your.email@example.com>
2026-08-10 20:32:56 +0500

    starting the project

---
+++ cmd/web/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	//w.Write([]byte("Display a specific snippet..."))
+
+	fmt.Fprintf(w, "Display a specific snippet with ID: %d", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		//w.WriteHeader(http.StatusMethodNotAllowed)
+		//w.Write([]byte("Method not allowed"))
+		return
+	}
+
+	w.Write([]byte("Create a new snippet..."))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Print("Starting server on: 8080")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
